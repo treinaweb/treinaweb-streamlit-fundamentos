94c1cd08541546425819da215fee9b5159e78361
Rodrigo <rcbm539@gmail.com>
2022-08-15 11:37:15 -0300

    03:03 - Widgets III

--- 05-widgets.py
+++ 05-widgets.py
@@ -105,3 +105,35 @@ st.header("Utilizando Time Input")
 
 t = st.time_input('Set an alarm for', datetime.time(8, 45))
 st.write('Alarm is set for', t)
+
+st.header("Utilizando File Uploader")
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+     # To read file as bytes:
+     bytes_data = uploaded_file.getvalue()
+     st.write(bytes_data)
+
+     # To convert to a string based IO:
+     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+     st.write(stringio)
+
+     # To read file as string:
+     string_data = stringio.read()
+     st.write(string_data)
+
+     # Can be used wherever a "file-like" object is accepted:
+     dataframe = pd.read_csv(uploaded_file)
+     st.write(dataframe)
+
+st.header("Utilizando Câmera Input")
+
+picture = st.camera_input("Take a picture")
+
+if picture:
+     st.image(picture)
+
+st.header("Utilizando Color Picker")
+
+color = st.color_picker('Pick A Color', '#00f900')
+st.write('The current color is', color)
