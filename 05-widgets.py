c15fbfc1be5b89740fef78ae21eeb54e94fced92
Rodrigo <rcbm539@gmail.com>
2022-08-15 11:36:49 -0300

    03:02 - Widgets II

--- 05-widgets.py
+++ 05-widgets.py
@@ -54,3 +54,54 @@ if genero == 'Comédia':
 else:
     st.write("Você não gosta de comédia")
 
+# Vídeo 2
+
+st.header("Utilizando MultiSelect")
+
+opcoes = st.multiselect(
+    'Quais suas cores preferidas?',
+    ['Verde', 'Amarelo', 'Vermelho', 'Azul', 'Preto']
+)
+
+st.write('Você selecionou: ', opcoes)
+
+st.header("Utilizando Slider")
+
+idade = st.slider('Quantos anos você tem?', 0, 100, 30)
+st.write("Eu tenho ", idade, " anos de vida")
+
+st.header("Utilizando o Select Slider")
+
+cor = st.select_slider(
+    'Selecione a cor',
+    options=['red', 'orange', 'yellow', 'green', 'blue', 'grey'])
+
+st.write('Cor favorita é: ', cor)
+
+st.header("Number Input")
+
+numero = st.number_input("Insira um número")
+st.write("Número digitado foi: ", numero)
+
+st.header("Utilizando o TextArea")
+
+txt = st.text_area('Text to analyze', '''
+     It was the best of times, it was the worst of times, it was
+     the age of wisdom, it was the age of foolishness, it was
+     the epoch of belief, it was the epoch of incredulity, it
+     was the season of Light, it was the season of Darkness, it
+     was the spring of hope, it was the winter of despair, (...)
+     ''')
+st.write('O texto é:', txt)
+
+st.header("Utilizando Date Input")
+
+d = st.date_input(
+     "When's your birthday",
+     datetime.date(2019, 7, 6))
+st.write('Your birthday is:', d)
+
+st.header("Utilizando Time Input")
+
+t = st.time_input('Set an alarm for', datetime.time(8, 45))
+st.write('Alarm is set for', t)
