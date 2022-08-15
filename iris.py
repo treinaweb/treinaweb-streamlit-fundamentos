1e1981afdbd8114b3c939fed2b71be4a8ee0cb2e
Rodrigo <rcbm539@gmail.com>
2022-08-15 11:42:26 -0300

    05:02 - Interação com os Dados

--- iris.py
+++ iris.py
@@ -7,6 +7,25 @@ st.header("Iris Flower Prediction App")
 
 st.sidebar.header("UI Parameters")
 
+def ui_parameters():
+    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.0)
+    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.2)
+    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 2.5)
+    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.5)
+
+    data = {
+        'sepal_length' : sepal_length,
+        'sepal_width'  : sepal_width,
+        'petal_length' : petal_length,
+        'petal_width'  : petal_width
+    }
+    parametros = pd.DataFrame(data, index=[0])
+    return parametros
+
+df = ui_parameters()
+
+st.subheader("UI parâmetros")
+st.write(df)
 
 iris = datasets.load_iris()
 
